b032a3df43521e016cebdf02ef394d06eb44ad8c
Yoesmar <yoesmarlozamolina@outlook.com>
2021-11-07 21:50:52 -0400

    Initial commit

---
+++ js/funtion.js
@@ -0,0 +1,23 @@
+let myevent=document.getElementById("event")
+myevent.addEventListener("mouseover",function(){
+    myevent.style.background=" rgba(226, 44, 162, 0.562)";
+});
+myevent.addEventListener("mouseleave",function(){
+    myevent.style.background="rgba(40, 241, 191, 0.600)";
+});
+
+let myevent1=document.getElementById("event1")
+myevent1.addEventListener("mouseover",function(){
+    myevent1.style.background=" rgba(226, 34, 162, 0.562)";
+});
+myevent1.addEventListener("mouseleave",function(){
+    myevent1.style.background="rgba(40, 241, 191, 0.600)";
+});
+
+let myevent2=document.getElementById("event2")
+myevent2.addEventListener("mouseover",function(){
+    myevent2.style.background=" rgba(226, 34, 162, 0.562)";
+});
+myevent2.addEventListener("mouseleave",function(){
+    myevent2.style.background="rgba(40, 241, 191, 0.600)";
+});
